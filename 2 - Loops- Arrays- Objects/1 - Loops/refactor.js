617a62b57a5982fffc9ec1113549dbca35011b3b
vnscriptkid <vnscriptkid@gmail.com>
2017-09-25 21:44:29 +0700

    Update refactor.js

--- 2 - Loops- Arrays- Objects/1 - Loops/refactor.js
+++ 2 - Loops- Arrays- Objects/1 - Loops/refactor.js
@@ -1,13 +1,15 @@
 "use strict"
 
+var red, green, blue, rgbColor, html;
+
 for (var i = 1; i <= 10; i++){
-  var red = Math.floor(Math.random() * 256);
-  var green = Math.floor(Math.random() * 256);
-  var blue = Math.floor(Math.random() * 256);
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
 
-  var rgbColor = red + ',' + green + ',' + blue;
+  rgbColor = red + ',' + green + ',' + blue;
 
-  var html = '<div style="width:30px;height:30px;background-color:rgb('+rgbColor+')"></div>'
+  html = '<div style="width:30px;height:30px;background-color:rgb('+rgbColor+')"></div>'
 
 
   document.write(html);
