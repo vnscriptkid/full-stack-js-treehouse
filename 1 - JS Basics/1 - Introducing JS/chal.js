58e3cb80ebf10186cedd4c16940414575164d756
vnscriptkid <vnscriptkid@gmail.com>
2017-09-25 15:51:34 +0700

    Create chal.js

---
+++ 1 - JS Basics/1 - Introducing JS/chal.js
@@ -0,0 +1,9 @@
+console.log('start the program');
+
+document.write('<h1>This is the biggest heading</h1>');
+document.write('<p>This is a paragraph</p>');
+document.write('<p>The font size is little smaller</p>');             
+
+alert('this line will be appeared in a small window');
+
+console.log('end the program');
