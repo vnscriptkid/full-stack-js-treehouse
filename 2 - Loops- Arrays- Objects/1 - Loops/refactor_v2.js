d0a33a0d42ccc9d7199d2c16b7e7817d3109d8ba
vnscriptkid <vnscriptkid@gmail.com>
2017-09-25 21:51:15 +0700

    Update refactor_v2.js

--- 2 - Loops- Arrays- Objects/1 - Loops/refactor_v2.js
+++ 2 - Loops- Arrays- Objects/1 - Loops/refactor_v2.js
@@ -1,19 +1,23 @@
 "use strict"
 
-var rgbColor, html;
+var rgbColor, html ='';
 
 function randomColor(){
   return Math.floor(Math.random() * 256);
+}
+
+function printColors(html){
+  document.write(html);
 }
 
 for (var i = 1; i <= 10; i++){ 
 
   rgbColor = randomColor() + ',' + randomColor() + ',' + randomColor();
 
-  html = '<div style="width:30px;height:30px;background-color:rgb('+rgbColor+')"></div>'
-
-
-  document.write(html);
+  html += '<div style="width:30px;height:30px;background-color:rgb('+rgbColor+')"></div>'
+  
 }
+
+printColors(html);
 
 
