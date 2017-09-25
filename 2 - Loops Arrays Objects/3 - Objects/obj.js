aff25132cf040bf1395558a5412c0ca6c1387f49
vnscriptkid <vnscriptkid@gmail.com>
2017-09-25 23:56:51 +0700

    Create obj.js

---
+++ 2 - Loops Arrays Objects/3 - Objects/obj.js
@@ -0,0 +1,16 @@
+"use strict"
+
+var students = [
+  {
+    name : 'thanh',
+    track: ['ios', 'design', 'java'],
+    achievements: 3,
+    points: 500
+  },
+  {
+    name : 'thao',
+    track: ['js', 'security'],
+    achievements: 1,
+    points: 200
+  }
+]
